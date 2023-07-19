0d70b7a20b121789b76e9485694de8233f200466
zak <mzhussain092@gmail.com>
2023-07-19 15:45:15 +0530

    add clerk authentication

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+﻿import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+
+export default function DashboardPage() {
+  return (
+    <div className=" text-3xl">
+      <p>Dashboard Page</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+﻿import { FC } from "react";
+
+interface LandingPageProps {}
+
+const LandingPage: FC<LandingPageProps> = ({}) => {
+  return <div>Landing Page</div>;
+};
+
+export default LandingPage;
